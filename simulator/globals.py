d6623dfbc5310e9fe4581f632fe4bba6d0773487
Thomas <thoso@student.sdu.dk>
2021-02-02 14:45:42 +0100

    Remove test files

--- simulator/globals.py
+++
@@ -1,7 +0,0 @@
-from numba import njit
-'''Script for initializing global variables in example script'''
-
-@njit
-def initialize():
-    global a;global b;global type;global epsilon;
-    a = 0; b = 1; type = 'default'; epsilon = 1
