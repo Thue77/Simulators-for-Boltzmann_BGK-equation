Added heterogenity in one-step method. Derivtive of diffusion coefficient was missing. Also corrected calculation of sum of squares in example for figure 5. Next commit will contain the results of the test

simulator/example_script.py
@@ -56,6 +56,12 @@ def R(x):
     elif type == 'B1':
         return -b*(a*(x-1)-1)*(x<=1) + b*(a*(x-1)+1)*np.logical_not(x<=1)
 
+def dR(x):
+    if type == 'default':
+        return 0
+    elif type == 'B1':
+        return (x<=1)*(-b*a) + (x>1)*(b*a)
+
 #Anti derivative of R
 def R_anti(x):
     if type == 'default':
@@ -235,18 +241,22 @@ def KDML_cor_test_fig_5(N):
     dt_list = 1/2**np.arange(0,22,1)
     runs = int(max(N/cache,1))
     n = int(N/cache) if N>cache else N
-    V = np.zeros((runs,len(dt_list)))#; E = np.zeros(len(dt_list))
-    V_d = np.zeros((runs,len(dt_list)-1))#; E_bias = np.zeros(len(dt_list)-1)
+    V = np.zeros((runs,len(dt_list))); E = np.zeros((runs,len(dt_list)))
+    V_d = np.zeros((runs,len(dt_list)-1)); E_bias = np.zeros((runs,len(dt_list)-1))
     if runs >1:
         for r in prange(runs):
             x0,v0,v_l1_next = Q(n)
             for i in prange(len(dt_list)-1):
                 # print(dt_list[i])
-                x_f,x_c = KD_C(dt_list[i+1],dt_list[i],x0,v0,v_l1_next,0,1,mu,sigma,M,R,SC,R_anti=R_anti)
-                V_d[r,i] = np.sum((x_f-x_c-np.mean(x_f-x_c))**2)
-                V[r,i] = np.sum((x_c-np.mean(x_c))**2)
-                if i == len(dt_list)-2: V[r,i+1] = np.sum((x_f-np.mean(x_f))**2)
-        return compute_mean_alongaxis(V)/(N-1),compute_mean_alongaxis(V_d)/(N-1)
+                x_f,x_c = KD_C(dt_list[i+1],dt_list[i],x0,v0,v_l1_next,0,1,mu,sigma,M,R,SC,R_anti=R_anti,dR=dR)
+                E_bias[r,i] = np.mean(x_f-x_c)
+                V_d[r,i] = np.sum((x_f-x_c-E_bias[r,i])**2)
+                E[r,i] = np.mean(x_c)
+                V[r,i] = np.sum((x_c-E[r,i])**2)
+                if i == len(dt_list)-2:
+                    E[r,i+1] = np.mean(x_f)
+                    V[r,i+1] = np.sum((x_f-E[r,i+1])**2)
+        return add_sum_of_squares_alongaxis(V,E,cache)/(N-1),add_sum_of_squares_alongaxis(V_d,E_bias,cache)/(N-1)
     else:
         x0,v0,v_l1_next = Q(N)
         for i in prange(len(dt_list)-1):
@@ -255,7 +265,7 @@ def KDML_cor_test_fig_5(N):
             V_d[0,i] = np.var(x_f-x_c)#np.sum((x_f-x_c-np.mean(x_f-x_c))**2)
             V[0,i] = np.var(x_c)#np.sum((x_c-np.mean(x_c))**2)
             if i == len(dt_list)-2: V[0,i+1] = np.var(x_f)#np.sum((x_f-np.mean(x_f))**2)
-        return V[0,:]/(N-1),V_d[0,:]/(N-1)
+        return V[0,:],V_d[0,:]
 
 
 @njit(nogil=True,parallel=True)
@@ -268,6 +278,25 @@ def compute_mean_alongaxis(A,axis=0):
         for i in prange(n):
             mu[j] = mu[j] + 1/(i+1)*(A[i,j]-mu[j])
     return mu
+
+@njit(nogil=True,parallel=True)
+def add_sum_of_squares_alongaxis(A,A_mean,N,axis=0):
+    '''If axis is zero then the mean of each coulmn is calculated
+    A: matrix of sum of squares for each run
+    A_mean: matrix of means for each run
+    N: number of paths used in each run
+    '''
+    n = A.shape[axis]
+    m = A.shape[0] if axis==1 else A.shape[1]
+    ss = np.zeros(m)
+    for j in prange(m):
+        mu_old = 0
+        for i in prange(n):
+            Delta = A_mean[i,j] - mu_old
+            M = (N*(N*(i)))/(N+N*(i))
+            ss[j] = ss[j] + A[i,j] + Delta**2*M
+            mu_old = mu_old + Delta*(N*i)/(N+N*(i))
+    return ss
 
 
 def plot_var(V,V_d):
@@ -330,9 +359,9 @@ if __name__ == '__main__':
         start = time.time()
         V,V_d = KDML_cor_test_fig_5(int(sys.argv[6]))
         print(f'elapsed time is {time.time()-start}')
-        np.savetxt(f'var_a_{a}_b_{b}_type_{type}.txt',np.vstack((V,np.append(V_d,0))))
+        # np.savetxt(f'var_a_{a}_b_{b}_type_{type}.txt',np.vstack((V,np.append(V_d,0))))
         # print(f'V: {V}')
-        # plot_var(V,V_d)
+        plot_var(V,V_d)
 
 
 

simulator/kinetic_diffusion/one_step.py
@@ -10,11 +10,18 @@ def __psi_k(tau,x,v,t):
 
 #Diffusive coefficient
 def __D(x,v,e,theta,mu,sigma,R):
-    return 2*sigma(x)**2/(R(x)**2)*(2*(e-1)+R(x)*theta*(e+1))+(v-mu(x))**2/(R(x)**2)*(1-2*R(x)*theta*e-e**2)
+    return sigma(x)**2/(theta*R(x)**2)*(2*(e-1)+R(x)*theta*(e+1))+(v-mu(x))**2/(2*theta*R(x)**2)*(1-2*R(x)*theta*e-e**2)
+
+#Derivative of diffusion coefficient w.r.t. R
+def dDdR(x,v,e,theta,mu,sigma,R):
+    #Derivative of first term:
+    D1 = (sigma(x)**2/(R(x)**2)*(1-e-R(x)*theta*e)  -2*sigma(x)**2/(theta*R(x)**3)*(R(x)*theta+2*e-2+R(x)*theta*e))
+    D2 = (v-mu(x))**2*(1/(2*R(x)**2)*(2*e**2-2*e+2*R(x)*theta*e)-1/(theta*R(x)**3)*(1-e**2-2*R(x)*theta*e))
+    return D1+D2
 
 #The KD-operator. Moves the particle(s) according to the kinetic-diffusion algorithm by a distance of dt
 #NOT ADAPTED TO HETEROGENOUS BG!!
-def __psi_d(xp,t,v_next,theta,xi,mu,sigma,R):
+def __psi_d(xp,t,v_next,theta,z,mu,sigma,R,dR=None):
     '''
     dt: the time step
     v0: initial velocity for kinetc part of the step
@@ -26,20 +33,20 @@ def __psi_d(xp,t,v_next,theta,xi,mu,sigma,R):
     n = len(xp)
     # dt = np.ones(n)*dt
     # theta = dt - np.mod(tau,dt)
-    x_pp = xp + mu(xp)*theta/2 #Intermediate point to deal with heterogenity
+    x_pp = xp# + mu(xp)*theta/2 #Intermediate point to deal with heterogenity
     v = v_next.copy() #Velocity of kinetic phase in next step. Drawn at the position of the collision
     e = np.exp(-R(x_pp)*theta)
-    dVdx_r = 0#derivative(__D,x_pp,dx=1e-6,args = (v,e,theta,mu,sigma,R))
-    Edx = mu(x_pp)*theta + (v-mu(x_pp))*1/R(x_pp)*(1-e) + 0.5*dVdx_r #Equation 31
-    Vdx = __D(x_pp,v,e,theta,mu,sigma,R)
-    Vdx = np.maximum(Vdx,0) #To avoid numerical errors
-    x = xp + Edx + np.sqrt(Vdx)*xi #Update postion by diffusive motion
+    dDdR_dRdx = np.zeros(n) if dR is None else dDdR(x_pp,v,e,theta,mu,sigma,R)*dR(x_pp)
+    A = mu(x_pp) + (v-mu(x_pp))*1/(theta*R(x_pp))*(1-e) + dDdR_dRdx #Equation 31
+    D = __D(x_pp,v,e,theta,mu,sigma,R)
+    D = np.maximum(D,0) #To avoid numerical errors
+    x = xp + A*theta + np.sqrt(2*D*theta)*z #Update postion by diffusive motion
     t = t + theta
     return x,v,t
 
 
 #Kinetic-diffusion opertor
-def phi_KD(dt,x0,v0,t,tau,xi,mu,sigma,M,R,v_rv=None):
+def phi_KD(dt,x0,v0,t,tau,z,mu,sigma,M,R,v_rv=None,dR=None):
     '''
     dt: step size
     x0: initial positions
@@ -56,7 +63,7 @@ def phi_KD(dt,x0,v0,t,tau,xi,mu,sigma,M,R,v_rv=None):
         step by using the ones from the fine step. In that case v_rv is given'''
     v_norm = M(xp) if v_rv is None else v_rv
     v_next = mu(xp)+sigma(xp)* v_norm
-    x,v,t = __psi_d(xp,t,v_next,theta,xi,mu,sigma,R)
+    x,v,t = __psi_d(xp,t,v_next,theta,z,mu,sigma,R,dR=dR)
     return x,v,t,v_norm
 
 jit_module(nopython=True,nogil=True, parallel = True)

simulator/test_programs/poisson.py
@@ -19,7 +19,8 @@ tau2 = np.zeros(numbers)
 for i in range(numbers):
     U = np.random.uniform(0, 1, K)
     T = dt*np.sort(U)
-    tau2[i] = T[5]-T[4]
+    tau2[i] = T[5] -T[4]
+
 
 def tau_K(K,dt,t):
     return K*(dt-t)**(K-1)/dt**K