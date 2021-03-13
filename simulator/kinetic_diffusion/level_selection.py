ba05b8842107c26bf4ed81b5083881a6c0febb9d
Thomas <thoso@student.sdu.dk>
2021-03-13 16:01:14 +0100

    Attempt at level selection added. Not tested!

--- simulator/kinetic_diffusion/level_selection.py
+++ simulator/kinetic_diffusion/level_selection.py
@@ -1,1 +1,18 @@
 import numpy as np
+
+def select_levels(V,V_d):
+    '''
+    V: array of variances. Length L+1
+    V_d: array of variances of bias. Length L
+    '''
+    l = 1
+    test = V_d > V[1:]
+    if np.sum(test)>1:
+        l = np.argwhere(test).flatten()[-1]+2 #Last index where variance of bias is larger than V
+    L = [l-1,l]
+    V_min = V_d[max(l-2,0)]
+    for j in range(l,len(V_d)):
+        if V_d[j]<V_min/2:
+            L += [j]
+            V_min = V_d[j]
+    return L
