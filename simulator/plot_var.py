322d36f757b8cf8e2a97668dc201f8a2c03ebcfa
Thomas <thoso@student.sdu.dk>
2021-04-02 15:27:37 +0200

    Add axis to vr_KDMCML plots

--- simulator/plot_var.py
+++ simulator/plot_var.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import math
+import os
 
 A = [0.0,0.1,0.2,0.5,1.0,2.0,5.0,10.0,20.0]
 B = [1.0,10.0,100.0,1000.0]
@@ -11,12 +12,14 @@ dt_list = 1/2**np.arange(0,22,1)
 
 # fig,axes = plt.subplot(nrow=2,ncol=2)
 plt.figure(1)
-
+script_dir = os.path.dirname(__file__) #<-- absolute dir the script is in
 for i,b in enumerate(B):
     index = 221 + i
     plt.subplot(index)
     for a in A:
-        data = np.loadtxt(f'var_a_{a}_b_{b}_type_B1.txt')
+        rel_path = f'KD_var_results/var_a_{a}_b_{b}_type_B1.txt'
+        abs_file_path = os.path.join(script_dir, rel_path)
+        data = np.loadtxt(abs_file_path)
         V = data[0]
         V_d = data[1]
         # PLot slop marker
@@ -39,6 +42,8 @@ for i,b in enumerate(B):
     plt.title(f'b = {b}')
     plt.xscale('log')
     plt.yscale('log')
+    plt.xlabel('\u0394 t')
+    plt.ylabel('variance')
 # plt.legend(loc=9)
 # plt.legend(bbox_to_anchor=(0., 1.02, 1., .102), loc=0)
 plt.legend(bbox_to_anchor=(-0.6,2.3,1,0.2), loc="upper center",
