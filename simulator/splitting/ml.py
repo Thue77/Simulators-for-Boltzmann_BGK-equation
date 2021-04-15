543f8b71e8458d16aa6697a34e35a614cecaf020
Thomas <thoso@student.sdu.dk>
2021-04-15 10:26:13 +0200

    explanations for multilevel APS method

--- simulator/splitting/ml.py
+++ simulator/splitting/ml.py
@@ -2,4 +2,15 @@ import numpy as np
 from .correlated import correlated
 
 
-def ml(e2,Q,t0,T,M_t,eps,Q,M,r)
+def ml(e2,Q,t0,T,M_t,eps,M,r,F):
+    '''
+    e2: bound on mean square error
+    Q: initial distribution
+    t0: starting time
+    T: end time
+    M_t: dt_c/dt_f, where dt_c is coarse step size and dt_f is fine step size
+    eps: mean free path, i.e. epsilon in model
+    M: equilibrium distribution for velocity
+    r: collision rate
+    F: function used to find quantity of interest, E(F(X,V)). 
+    '''
