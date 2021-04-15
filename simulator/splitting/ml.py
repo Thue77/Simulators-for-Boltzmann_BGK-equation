a4f96292b72ddc628440372e4b675c67a940888d
Thomas <thoso@student.sdu.dk>
2021-04-15 12:40:28 +0200

    Level selection for APS

--- simulator/splitting/ml.py
+++ simulator/splitting/ml.py
@@ -2,6 +2,29 @@ import numpy as np
 from .correlated import correlated
 
 
+def select_levels(t0,T,M_t,strategy=1,cold_start=True,N=100):
+    '''
+    strategy: indicates if strategy 1 or 2 is used
+    cold_start: indicates if the variance and estimates are calculated for an
+    initial number of paths
+    '''
+    levels = []
+    if strategy==1:
+        levels += [eps**2]
+        levels += [eps**2/M_t]
+        N_out=np.ones(2)*N
+        N_diff=np.ones(2)*N
+        V_out = np.zeros(2);C_out = np.zeros(2);
+    elif strategy==2:
+        levels += [T-t0]
+        levels += [eps**2]
+        levels += [eps**2/M_t]
+        N_out=np.ones(3)*N
+        N_diff=np.ones(3)*N
+        V_out = np.zeros(3);C_out = np.zeros(3);
+    return levels,N_out,Q_out,V_out,C_out
+
+
 def ml(e2,Q,t0,T,M_t,eps,M,r,F):
     '''
     e2: bound on mean square error
@@ -12,5 +35,5 @@ def ml(e2,Q,t0,T,M_t,eps,M,r,F):
     eps: mean free path, i.e. epsilon in model
     M: equilibrium distribution for velocity
     r: collision rate
-    F: function used to find quantity of interest, E(F(X,V)). 
+    F: function used to find quantity of interest, E(F(X,V)).
     '''
