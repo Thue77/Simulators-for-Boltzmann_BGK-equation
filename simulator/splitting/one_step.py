549cf022988c2bc947c795b24e955518e5e35971
Thomas <thoso@student.sdu.dk>
2021-04-17 10:54:20 +0200

    Remove comments

--- simulator/splitting/one_step.py
+++ simulator/splitting/one_step.py
@@ -4,13 +4,7 @@ from numba import njit,jit_module,prange
 
 
 def psi_t(x,v,dt,eps,z,r):
-    # print(f'v in transport term: {v}')
-    # print(dt)
-    # D = (v*eps/(eps**2+dt*r))**2*dt/(eps**2+dt*r)
-    # return x + v*eps/(eps**2+dt*r)*dt +   np.sqrt(2*dt)*np.sqrt(D)*z#np.sqrt(2*(v*eps/(eps**2+dt*r))**2*dt**2/(eps**2+dt*r))*z
-    '''The characteristic velocity is not included in the diffusion coefficient
-    and must be divided out'''
-    D = v**2*dt/(eps**2+dt*r(x))#(v*(eps**2+dt*r)/eps)**2*dt/(eps**2+dt*r)
+    D = v**2*dt/(eps**2+dt*r(x))
     A = eps/(eps**2+dt*r(x))*v
     return x + A*dt + np.sqrt(2*dt*D)*z#v*dt + np.sqrt(2*dt*D)*z
 
@@ -19,28 +13,14 @@ def psi_c(x,v,dt,eps,u,B,r,v_bar = None):
     u: vector of uniform numbers in [0,1] to sample from M with appropriate probability
     '''
     p = (u>=eps**2/(eps**2+dt*r(x))) #1 if collision occurs
-    # print(f'dt: {dt}, prob: {1-eps**2/(eps**2+dt*r)}')
     if v_bar is None:
         _,v_bar = B(x)
 
-    v = (1-p)*v + p*v_bar#v_char(dt,eps,v_tilde)*p*v_bar
-    # print(f'v_bar: {v_bar}, v: {v}')
-    # if dt ==1: print(1-p)
+    v = (1-p)*v + p*v_bar
     return v,v_bar
 
-# def v_char(dt,eps,v_tilde=1,r=1):
-#     '''
-#     v_tilde is the standard deviation of the post-collisional distribution
-#     '''
-#     return eps/(eps**2+dt*r)*v_tilde
-
 def phi_APS(x,v,dt,eps,z,u,B,r,v_next=None,boundary=None):
-    # print(f'x= {x} \n v= {np.var(v)}')
     x = psi_t(x,v,dt,eps,z,r)
-    # if sigma is not None:
-    #     v_tilde = sigma(x)*eps
-    # else:
-    #     v_tilde=1
     if boundary is not None:
         x = boundary(x)
     v,v_bar = psi_c(x,v,dt,eps,u,B,r,v_next)
@@ -58,7 +38,7 @@ def phi_standard(x,v,dt,eps,M,r,boundary):
     return x,v
 
 
-# jit_module(nopython=True,nogil=True)
+jit_module(nopython=True,nogil=True)
 
 
 
