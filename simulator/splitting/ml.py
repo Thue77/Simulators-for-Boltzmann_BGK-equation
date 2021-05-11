9770c25fd65677e0c57fc3b7860d1cd7a01f419a
Thomas <thoso@student.sdu.dk>
2021-05-11 09:54:59 +0200

    New test for bias and ml_test. ml_test seems to work

--- simulator/splitting/ml.py
+++ simulator/splitting/ml.py
@@ -4,8 +4,12 @@ from .mc import mc
 from .AddPaths import delta,x_hat,Sfunc
 import time
 from numba import njit,jit_module,prange,objmode
+import sys
+from datetime import datetime
+import matplotlib.pyplot as plt
+from scipy.stats import kurtosis
 
-# @njit(nogil=True)
+@njit(nogil=True)
 def select_levels(t0,T,M_t,eps,F,strategy=1,cold_start=True,N=100,boundary=None):
     '''
     strategy: indicates if strategy 1 or 2 is used
@@ -13,17 +17,14 @@ def select_levels(t0,T,M_t,eps,F,strategy=1,cold_start=True,N=100,boundary=None)
     initial number of paths
     '''
     if strategy==1:
-        dt_0 = np.minimum(eps**2,0.025)
-        levels = [dt_0]
-        levels += [dt_0/M_t]
+        dt_0 = np.minimum((eps/10)**2,0.025)
+        levels = np.array([dt_0,dt_0/M_t])
         N_out=np.zeros(2,dtype=np.int64)
         N_diff=np.ones(2,dtype=np.int64)*N
         SS_out = np.zeros(2);C_out = np.zeros(2); E_out=np.zeros(2)
     else:
-        levels = [float(T-t0)]
-        dt_1 = np.minimum(eps**2,0.025)
-        levels += [dt_1]
-        levels += [dt_1/M_t]
+        dt_1 = np.minimum((eps/10)**2,0.025)
+        levels = np.array([float(T-t0),dt_1,dt_1/M_t])
         N_out=np.zeros(3,dtype=np.int64)
         N_diff=np.ones(3,dtype=np.int64)*N
         SS_out = np.zeros(3);C_out = np.zeros(3); E_out=np.zeros(3)
@@ -32,6 +33,7 @@ def select_levels(t0,T,M_t,eps,F,strategy=1,cold_start=True,N=100,boundary=None)
 
 @njit(nogil=True,parallel=True)
 def update_paths(I,E,SS,C,N,N_diff,levels,t0,T,M_t,eps,Q,M,r,F,boundary,strategy=1):
+    # levels = ll.copy()
     for j in prange(len(I)):
         i=I[j]
         # print(i)
@@ -73,8 +75,8 @@ def update_paths(I,E,SS,C,N,N_diff,levels,t0,T,M_t,eps,Q,M,r,F,boundary,strategy
     return E,SS,N,N_diff,C
 
 
-# @njit(nogil=True)
-def ml(e2,Q,t0,T,M_t,eps,M,r,F,N_warm=40,boundary=None,strategy=1):
+@njit(nogil=True)
+def ml(e2,Q,t0,T,M_t,eps,M,r,F,N_warm=40,boundary=None,strategy=1,alpha=None,beta=None,gamma=None):
     '''
     e2: bound on mean square error
     Q: initial distribution
@@ -108,8 +110,16 @@ def ml(e2,Q,t0,T,M_t,eps,M,r,F,N_warm=40,boundary=None,strategy=1):
             V = SS/(N-1) #Update variance
             '''Determine number of paths needed with new information'''
             N_diff = np.ceil(2/e2*np.sqrt(V/C)*np.sum(np.sqrt(V*C))).astype(np.int64) - N
-        '''Test bias is below e2/2'''
-        test = max(abs(0.5*E[L-2]),abs(E[L-1])) < np.sqrt(e2/2)
+        '''Test bias is below e2/2 - to do so it is necessary to know exponent
+        in weak error'''
+        # test = max(abs(0.5*E[L-2]),abs(E[L-1])) < np.sqrt(e2/2)
+        if E.size>=4:
+            if alpha is None:
+                L1 = max(1,np.where(levels<=eps**2)[0][0])
+                pa = lin_fit(np.arange(L1,L),np.log2(np.abs(E[L1:L])),1); alpha = pa[0]
+            test = np.max(np.abs(E[-3:])/M_t**(np.flip(np.arange(0,3))*alpha))/(M_t**alpha-1) < np.sqrt(e2/2)
+        else:
+            test=False
         if test:
             break
         L += 1;
@@ -121,9 +131,174 @@ def ml(e2,Q,t0,T,M_t,eps,M,r,F,N_warm=40,boundary=None,strategy=1):
         E = np.append(E,0.0); V = np.append(V,0.0); SS = np.append(SS,0.0)
         C = np.append(C,0.0);#np.append(C,M_t**(L-1)+M_t**(L-2)); #
         if strategy==1:
-            levels += [levels[0]/(M_t**(L-1))]
+            levels = np.append(levels[0]/(M_t**(L-1)))
         else:
-            levels += [levels[1]/(M_t**(L-2))]
+            levels = np.append(levels,levels[1]/(M_t**(L-2)))
     return E,V,C,N,levels
 
-jit_module(nopython=True,nogil=True)
+
+def lin_fit(x,y):
+    p=1; n = x.size
+    X = np.stack((np.ones(n),x),axis=1)
+    normal_matrix = np.matmul(X.T,X)
+    moment_matrix = np.matmul(X.T,y)
+    return np.matmul(np.linalg.inv(normal_matrix),moment_matrix)[1]
+
+
+@njit(nogil=True,parallel=True)
+def convergence_tests(N,dt_list,Q,t0,T,M_t,eps,M,r,F,boundary,strategy):
+    '''Calculates values for consistency test for each level given by dt_list'''
+    cores = 8 #Controls parrelisation
+
+    L = dt_list.size
+    b = np.zeros(L) #mean(F(x^f)-F(X^c))
+    b2 = np.zeros(L) #mean((F(x^f)-F(X^c))^2)
+    b3 = np.zeros(L) #mean((F(x^f)-F(X^c))^3)
+    b4 = np.zeros(L) #mean((F(x^f)-F(X^c))^4)
+    v = np.zeros(L) #mean(F(X))
+    v2 = np.zeros(L) #mean(F(X)^2)
+    var1 = np.zeros(L) #var(F(X))
+    var2 = np.zeros(L) #var(F(X^f)-F(X^c))
+    # kur1 = np.empty(L-1) #kurtosis calculated for biases
+    cons = np.zeros(L) #consistency calculated for all biases
+    cost1 = np.zeros(L) #cost for each level
+    cost2 = np.zeros(L) #cost for each level bias
+
+
+
+    if N%cores!=0:
+        print('WARNING -  Number of samples is not divisble by 8!\n Please change N for optimal functionality')
+    n = round(N/cores)
+    # all_diff = np.zeros((N,L))
+
+    for l in range(L):
+        diff = np.empty((cores,n))
+        val = np.empty((cores,n))
+        for j in prange(cores):
+            if l<L-1:
+                # print(f'l={l}')
+                with objmode(start1 = 'f8'):
+                    start1 = time.perf_counter()
+                x_f,x_c = correlated(dt_list[l+1],M_t,t0,T,eps,n,Q,M,r,boundary=boundary)
+                with objmode(end1 = 'f8'):
+                    end1 = time.perf_counter()
+                cost2[l+1] += (end1-start1)
+                diff[j,:] = F(x_f)-F(x_c)
+            with objmode(start2 = 'f8'):
+                start2 = time.perf_counter()
+            x = mc(dt_list[l],t0,T,n,eps,Q,M,r,boundary=boundary)
+            with objmode(end2 = 'f8'):
+                end2 = time.perf_counter()
+            cost1[l] += (end2-start2)
+            val[j,:] = F(x)
+        cost1[l] = cost1[l]/N
+        v[l] = np.mean(val)
+        v2[l] = np.mean(val**2)
+        var1[l]  = v2[l] - v[l]**2
+        if l<L-1:
+            # all_diff[:,l+1] = diff.flatten()
+            b[l+1] = np.mean(diff)
+            b2[l+1] = np.mean(diff**2)
+            b3[l+1] = np.mean(diff**3)
+            b4[l+1] = np.mean(diff**4)
+            var2[l+1] = b2[l+1]-b[l+1]**2
+            cost2[l+1] = cost2[l+1]/N
+            cons[l+1] = np.abs(b[l+1]+v[l]-v[l+1])/(3*(np.sqrt(var2[l+1])+ np.sqrt(var1[l])+np.sqrt(var1[l+1])))/np.sqrt(N)
+    kur1 = np.append(0,(b4[1:]-4*b3[1:]*b[1:]+6*b2[1:]*b[1:]**2-3*b[1:]**4)/(b2[1:]-b[1:]**2)**2)
+    return b,b2,b3,b4,v,v2,var1,var2,kur1,cons,cost1,cost2#,all_diff
+
+
+# jit_module(nopython=True,nogil=True)
+
+
+
+
+def ml_test(N,N0,dt_list,E2,Q,t0,T,M_t,eps,M,r,F,logfile,boundary=None,strategy=None):
+    ''''
+    filename for logfile should always begin with 'logfile_APS'
+
+
+    '''
+    if logfile.name[0:11]!='logfile_APS':
+        sys.exit('ERROR: name of logfile should start with "logfile_APS"')
+    now = datetime.now().strftime("%d-%B-%Y %H:%M:%S")
+    logfile.write("\n")
+    logfile.write("*********************************************************\n")
+    logfile.write(f"***Python ml_test for APS method on {now}         ***\n")
+    logfile.write("\n")
+    logfile.write("*********************************************************\n")
+    logfile.write("*** Experiemnt setup  ***\n")
+    logfile.write("*** S(x,v) = 1/sqrt(2*pi)*v^2*e^{-v^2/2}*(1+cos(2*pi*(x+1/2))) ***\n")
+    logfile.write(f"*** r(x) = 1/eps^2 (ax+b), with a={(r(1)-10)/eps**2}, b=10 and eps = {eps}  ***\n")
+    if 2!=F(2):
+        logfile.write(f"*** OBS!! replace this text with specification of quantity of interest F(3) = {F(3)} ***\n")
+    else:
+        logfile.write("*** Quantity of interest is F(X) = X  ***\n")
+    if boundary is None:
+        logfile.write("*** No boundary conditions  ***\n")
+    else:
+        logfile.write("*** OBS!! replace this text with specification of boundary conditions  ***\n")
+    logfile.write("*********************************************************\n")
+    logfile.write("Convergence tests, kurtosis, telescoping sum check \n")
+    L = dt_list.size
+    logfile.write(f"*** using {N} samples and {L} levels ***\n")
+    logfile.write(" l dt^f mean(F(X^f)-F(X^c)) mean(F(X^c))  var(F(X^f)-F(X^c)) var(F(X^c))")
+    logfile.write(" cost(F(X^f)-F(X^c)) cost(F(X)) kurtosis consistency \n")
+    logfile.write("---------------------------------------------------------\n")
+
+    b,b2,b3,b4,v,v2,var1,var2,kur1,cons,cost1,cost2 = convergence_tests(N,dt_list,Q,t0,T,M_t,eps,M,r,F,boundary=boundary,strategy=strategy)
+
+    for i in range(dt_list.size):
+        logfile.write(f'{i} {dt_list[i]} {b[i]} {v[i]} {var2[i]} {var1[i]} {cost2[i]} {cost1[i]} {kur1[i]} {cons[i]}\n')
+
+    # Linear regression to estimate alpha, beta and gamma. Only test for dt << eps^2
+    L1 = np.where(dt_list<eps**2)[0][1]
+    pa = np.polyfit(range(L1,L),np.log2(np.abs(b[L1:L])),1); alpha = -pa[0]
+    # print(f'my alhpa: {lin_fit(np.arange(L1,L),np.log2(np.abs(b[L1:L])))}')
+    pb = np.polyfit(range(L1,L),np.log2(np.abs(var2[L1:L])),1); beta = -pb[0]
+    pg = np.polyfit(range(L1,L),np.log2(np.abs(cost2[L1:L])),1); gamma = pg[0]
+
+    logfile.write('\n*********************************************************\n')
+    logfile.write('\n*** Linear regression estimates of MLMC paramters ***\n')
+    logfile.write(f'\n*** regression is done for levels with dt << eps^2 = {eps**2} ***\n')
+    logfile.write('*********************************************************\n')
+    logfile.write(f'alpha = {alpha} (exponent for weak convergence) \n')
+    logfile.write(f'beta = {beta} (exponent for variance of bias estimate) \n')
+    logfile.write(f'gamma = {gamma} (exponent for cost of bias estimate) \n')
+
+
+    logfile.write("\n*********************************************************\n")
+    logfile.write("*** MLMC complexity test ***\n")
+    logfile.write("*********************************************************\n")
+    logfile.write(" e2 value mlmc_cost N_l \n")
+    logfile.write("---------------------------------------------------------\n")
+
+    for e2 in E2:
+        E,_,C,N,_ = ml(e2,Q,t0,T,M_t,eps,M,r,F,N0,boundary=boundary,strategy=strategy,alpha=alpha)
+        logfile.write(f" {e2} {np.sum(E)} {np.dot(C,N)} {N} \n")
+
+    logfile.write('\n')
+    np.savetxt('resultfile'+logfile.name[7:],(dt_list,v,b,var1,var2,cost1,cost2,kur1,cons))
+
+    logfile.close()
+
+
+    # plt.plot(dt_list[1:],var2[1:],':',label='var(F(x^f)-F(X^c))')
+    # plt.plot(dt_list,var1,'--',color = plt.gca().lines[-1].get_color(),label='var(F(X))')
+    # plt.plot(dt_list[1:],np.abs(b[1:]),':',label='mean(|F(x^f)-F(X^c)|)')
+    # plt.plot(dt_list,v,'--',color = plt.gca().lines[-1].get_color(),label='mean(F(X))')
+    # plt.title(f'Plot of variance and bias')
+    # plt.xscale('log')
+    # plt.yscale('log')
+    # plt.legend()
+    # plt.figure()
+    # plt.plot(range(1,L),kur1[1:],':',label='kurtosis')
+    # plt.title(f'Plot of kurtosis')
+    # plt.figure()
+    # plt.plot(range(1,L),cons[1:],':',label='kurtosis')
+    # plt.title(f'Plot check of consistency')
+    # plt.show()
+
+# def plot_results():
+#     '''Plots results from resultfile and if logfile is given then it finds the
+#     corresponding resultfile'''
