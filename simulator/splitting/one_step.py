dfc8dad825566ab5c384ffaa29852de86311f6aa
Thomas <thoso@student.sdu.dk>
2021-05-17 18:35:24 +0200

    Possibility to use diffusive coefficient from Emil's papaer is added

--- simulator/splitting/one_step.py
+++ simulator/splitting/one_step.py
@@ -3,8 +3,11 @@ from numba import njit,jit_module,prange
 import sys
 
 
-def psi_t(x,v,dt,eps,z,r):
-    D = v**2*dt/(eps**2+dt*r(x))#dt/(eps**2+dt*r(x))#
+def psi_t(x,v,dt,eps,z,r,diff=False):
+    if diff:
+        dt/(eps**2+dt*r(x))#
+    else:
+        D = v**2*dt/(eps**2+dt*r(x))#
     A = eps/(eps**2+dt*r(x))*v
     return x + A*dt + np.sqrt(2*dt*D)*z#v*dt + np.sqrt(2*dt*D)*z
 
@@ -19,16 +22,16 @@ def psi_c(x,v,dt,eps,u,B,r,v_bar = None):
     v = (1-p)*v + p*v_bar
     return v,v_bar
 
-def phi_APS(x,v,dt,eps,z,u,B,r,v_next=None,boundary=None):
-    x = psi_t(x,v,dt,eps,z,r)
+def phi_APS(x,v,dt,eps,z,u,B,r,v_next=None,boundary=None,diff=False):
+    x = psi_t(x,v,dt,eps,z,r,diff=diff)
     if boundary is not None:
         x = boundary(x)
     v,v_bar = psi_c(x,v,dt,eps,u,B,r,v_next)
     return x,v,v_bar
 
-def phi_APS_new(x,v,dt,eps,z,u,B,r,v_next=None,boundary=None):
+def phi_APS_new(x,v,dt,eps,z,u,B,r,v_next=None,boundary=None,diff=False):
     v,v_bar = psi_c(x,v,dt,eps,u,B,r,v_next)
-    x = psi_t(x,v,dt,eps,z,r)
+    x = psi_t(x,v,dt,eps,z,r,diff=diff)
     if boundary is not None:
         x = boundary(x)
     return x,v,v_bar
