448358523d7d11751879c5b072093c3d2b7a7472
Thomas <thoso@student.sdu.dk>
2021-05-18 10:48:57 +0200

    Files

--- simulator/test_density_num_exp_ml.bat
+++ simulator/test_density_num_exp_ml.bat
@@ -1,4 +1,5 @@
 @echo off
 REM script for running python script multiple times with arguments:
-python main.py 0.03 0 10 -de --paths 1000000 --folder Logfiles
-python main.py 0.3 1000 10 -de --paths 1000000 --folder Logfiles
+python main.py 0.5 0 10 -de --paths 600000 --folder Logfiles_new
+python main.py 0.03 0 10 -de --paths 600000 --folder Logfiles_new
+python main.py 0.3 1000 10 -de --paths 600000 --folder Logfiles_new
